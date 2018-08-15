c26b9edfcdbbb1478b110fb1979df1961a1cbacb
Diana G <dgarcia@inmegen.edu.gob.mx>
2018-08-15 17:05:42 -0500

    get only tsvs

--- 02_createIntraChrPlots_3D.R
+++ 02_createIntraChrPlots_3D.R
@@ -4,11 +4,11 @@ start <- Sys.time()
 cat("Starting at, ", format(start, "%H:%M:%OS3"), "\n" )
 
 setwd("~/Workspace/subtipos_mama")
+#setwd("/labs/csbig/subtipos_mama")
 
 cat("Loading data\n")
 load(file="genes.annot.RData")
 load(file="adjmatrix_basal.RData")
-
 
 all.genes.annot <- genes.annot
 chrs <- c(as.character(1:22), "X")
@@ -19,7 +19,6 @@ for (ch in chrs) {
   cat("Working with chromosome", ch, "\n")
   genes.annot <- all.genes.annot[all.genes.annot$Chr == ch, ]
   genes <- genes.annot$symbol
-  genes <- genes[1:10]
   g <- parallel::mclapply(X = genes, mc.cores = 4,  mc.cleanup = FALSE, FUN = function(gene1) {
     #cat("Gene: ", gene1, "\n")
     other.genes <- genes[which(genes != gene1)]
@@ -32,29 +31,20 @@ for (ch in chrs) {
                                 })
     matrix(unlist(mivals), ncol = 2, byrow = T)
   })
-  cat("Getting bin means.\n")
-  df <- plyr::ldply(g)
-  colnames(df) <- c("bin", "mi")
-  addtodf <- data.frame("bin" = 2, "mi"= c(0.7, 0.75, 0.5) )
-  df <- rbind(df, addtodf)
+  cat("Getting bin order.\n")
+  g <- plyr::ldply(g)
+  colnames(g) <- c("bin", "mi")
   
-  testy <- lapply(unique(df$bin), function(b) {
-    l <- df[df$bin == b, ]
+  o <- lapply(unique(g$bin), function(b) {
+    l <- g[g$bin == b, ]
     l$order <- order(l$mi)
     l
   })
-
-  colnames(dfmeans) <- c("distance", "avg.mi") 
-  dfmeans$distance <- dfmeans$distance*binlength
-  cat("Saving plot.\n")
-  png(paste("Basal_ch", ch, ".png", sep=""), width =800 , height = 400)
-  myplot <- ggplot(data=dfmeans, aes(x=distance, y=avg.mi, group=1)) +
-    geom_point(size = 1.5)
-  print(myplot)
-  dev.off() 
-  cat("Saving data.\n")
-  save(dfmeans, file=paste("basal_binmeans_ch", ch, ".RData", sep=""))
- 
+  
+  o <- plyr::ldply(o)
+  o <- o[order(o$bin, o$order),  ]
+  cat("Saving file.\n")
+  write.table(o, file=paste("basal_intra_order_",ch , ".tsv", sep=""), row.names = F, col.names = T, sep = "\t")
 }
 end <- Sys.time()
 cat("Whole thing took ", (end - start), "\n")
