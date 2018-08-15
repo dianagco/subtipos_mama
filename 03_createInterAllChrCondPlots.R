b732fa5c8db8eaa352e1f7feec635d8adc937f84
Diana G <dgarcia@inmegen.edu.gob.mx>
2018-08-15 14:56:39 -0500

    take only inter interactions

--- 03_createInterAllChrCondPlots.R
+++ 03_createInterAllChrCondPlots.R
@@ -8,20 +8,20 @@ cat("Starting at, ", format(start, "%H:%M:%OS3"), "\n" )
 setwd("/labs/csbig/subtipos_mama")
 #setwd("~/Workspace/subtipos_mama")
 
+chrs <- c(as.character(1:22), "X")
 conditions <- c("healthy", "luma", "lumb", "her2", "basal")
-
-g <- parallel::mclapply(X = conditions, mc.cores = 5,  mc.cleanup = FALSE, FUN = function(con) {
-  cat("Working with condition ", con, "\n")
-  cat("Loading data\n")
-  
-  load(file=paste("adjmatrix_", con, ".RData", sep="")) 
-  all.mis <- data.frame(mi = unlist(adjmatrix[!is.na(adjmatrix) & adjmatrix != 1]))
-  all.mis$condition <- con
-  colnames(all.mis) <- c("mi", "condition")
-  png(paste("All_", con, ".png", sep=""), width = 1200, height = 600)
-  myplot <- ggplot(data=all.mis) + 
-    geom_density(aes(x = mi, colour = myPalette(1)), show.legend = F) +
-    coord_cartesian(xlim = c(0, 0.25)) 
+l <- lapply(conditions, function(cond) {
+  g <- parallel::mclapply(X = chrs, mc.cores = 4,  mc.cleanup = FALSE, FUN = function(ch) {
+    cat("Working with chromosome ", ch, "\n")
+    cat("Loading data\n")
+    
+    load(file=paste("rdata/", cond, "/inter-take2/", cond, "_ch", ch, ".RData", sep="")) 
+  })
+  g <- plyr::ldply(g)
+  png(paste("Inter_", con, ".png", sep=""), width = 1200, height = 600)
+  myplot <- ggplot(data=g) + 
+      geom_density(aes(x = MI, colour = myPalette(1)), show.legend = F) +
+      coord_cartesian(xlim = c(0, 0.25)) 
   print(myplot)
   dev.off() 
 })
